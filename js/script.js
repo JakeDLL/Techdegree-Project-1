cc8c7f9641a6d23681a112f08bbd1556a9a8e698
Jacob D <35861176+JakeDLL@users.noreply.github.com>
2020-08-17 01:42:17 -0400

    Started quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,68 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: `"I know very little about anything, but I do know this: That if you can live your lfe without an audience, you should do it."`,
+    source: `Bo Burnham`,
+    citation: 'Make Happy',
+    year: 2016
+  },
+  {
+    quote: `"A kiss may ruin a human life."`,
+    source: `Oscar Wilde`,
+    citation: `A Woman of No Importance, Mrs. Arbuthnot, Act 4`,
+    year: 1893
+  },
+  {
+    quote: `"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that."`,
+    source: `Martin Luther King, Jr.`,
+    citation: `Strength to Love`,
+    year: 1963
+  },
+  {
+    quote: `"Wild men who caught and sang the sun in flight, And learn, too late, they grieved it on its way, Do not go gentle into that good night."`,
+    source: `Dylan Thomas`,
+    citation: `Botteghe Oscure`,
+    year: 1951
+  },
+  {
+    quote: `"We walk alone in the world. Friends, such as we desire, are dreams and fables."`,
+    source: `Ralph Waldo Emerson`,
+    citation: `Essays, First Series`,
+    year: 1841
+  },
+  {
+    quote: `"Wickedness is always easier than virtue; for it takes the short cut to everything."`,
+    source: `Samuel Johnson`,
+    citation: `The Journal of a Tour to the Hebrides`,
+    year: 1785
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  },
+  {
+    quote: ``,
+    source: ``,
+    citation: ``,
+    year: ``
+  }
+];
 
 
 /***
